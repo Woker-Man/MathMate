fd6e586366a7581c5bf20df604c2c19fc00124cc
Aryan <aryan.12a17@gmail.com>
2023-12-19 21:27:44 +0530

    dragDrop implemented
    completion of DragDro[

--- src/pages/DragDropAdd.jsx
+++ src/pages/DragDropAdd.jsx
@@ -1,184 +1,227 @@
-// src/pages/DragDropAdd.jsx
-import React, { useState, useEffect } from 'react';
-
-const appleImageUrl = 'https://purepng.com/public/uploads/large/purepng.com-red-appleapplemalus-domesticafruitdeliciousred-apple-170152716492043huf.png';
+import  { useEffect } from 'react';
+import './styles/DragDropAdd.css';
 
 const DragDropAdd = () => {
-  const [leftBoxImages, setLeftBoxImages] = useState([]);
-  const [rightBoxImages, setRightBoxImages] = useState([]);
-  const [basketImages, setBasketImages] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  var symbols = ["apple", "banana", "orange", "grape", "pencil"];
+  var targetFruit = "apple";
+  var basketCounter = 0;
+  var maxTargetCount = 0;
+  var score = 0;
+  var highScore = 0;
+  var draggedSymbol = null;
+  var currentSymbolIndex = 0;
 
-  const getRandomNumber = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
+  function shuffleSymbols() {
+    symbols.sort(() => Math.random() - 0.5);
+  }
 
-  const updateBoxes = () => {
-    let totalApples = getRandomNumber(2, 15); // Ensure at least one apple in each box
-    const minLeftCount = 1; // Minimum number of apples in the left box
-    const minRightCount = 1; // Minimum number of apples in the right box
-    const leftCount = getRandomNumber(minLeftCount, Math.min(totalApples - minRightCount, 15 - minRightCount));
-    const rightCount = totalApples - leftCount;
+  function createSymbol(symbol, count, container, movable) {
+    const symbolsPerRow = 3; // Number of symbols per row
+    const symbolSize = 50;
+    const padding = 30; // Increased padding for more space between symbols
+  
+    for (let i = 0; i < count; i++) {
+      const symbolElement = document.createElement("img");
+      symbolElement.className = "symbol";
+      symbolElement.src = `/images/${symbol}.png`; // Assuming all symbols are in the public directory
+      symbolElement.draggable = movable;
+  
+      symbolElement.addEventListener("dragstart", createDragStartHandler(symbolElement));
+  
+      const row = Math.floor(i / symbolsPerRow);
+      const col = i % symbolsPerRow;
+  
+      const posX = col * (symbolSize + padding);
+      const posY = row * (symbolSize + padding);
+  
+      symbolElement.style.top = posY + "px";
+      symbolElement.style.left = posX + "px";
+  
+      container.appendChild(symbolElement);
+    }
+  }
+  
 
-    const leftImages = Array(leftCount).fill(null).map((_, index) => ({ id: `${index}-${Date.now()}`, src: appleImageUrl }));
-    const rightImages = Array(rightCount).fill(null).map((_, index) => ({ id: `${index}-${Date.now()}`, src: appleImageUrl }));
+  function createDragStartHandler(element) {
+    return function (event) {
+      draggedSymbol = element;
+      event.dataTransfer.setDragImage(element, 0, 0);
+      draggedSymbol.style.transition = "none";
+    };
+  }
 
-    setLeftBoxImages(leftImages);
-    setRightBoxImages(rightImages);
-  };
+  function drag(event) {
+    if (draggedSymbol) {
+      draggedSymbol.style.left = event.clientX - draggedSymbol.width / 2 + "px";
+      draggedSymbol.style.top = event.clientY - draggedSymbol.height / 2 + "px";
+    }
+  }
+
+  function drop(event) {
+    event.preventDefault();
+    if (draggedSymbol && isApple(draggedSymbol)) {
+      symbols.splice(symbols.indexOf(targetFruit), 1);
+      basketCounter++;
+      document.getElementById("basket").textContent = "Basket: " + basketCounter;
+
+      draggedSymbol.style.transition = "transform 0.2s ease-in-out";
+      draggedSymbol.style.left = "0px";
+      draggedSymbol.style.top = "0px";
+      draggedSymbol.style.display = "none";
+
+      document.getElementById("basket-container").appendChild(draggedSymbol);
+      draggedSymbol = null;
+    }
+  }
+
+  function displayTemporaryMessage(message) {
+    var messageElement = document.createElement("div");
+    messageElement.className = "temporary-message";
+    messageElement.textContent = message;
+    document.body.appendChild(messageElement);
+
+    setTimeout(function () {
+      document.body.removeChild(messageElement);
+    }, 2000);
+  }
+
+  function isApple(element) {
+    var symbol = element.src.split("/").pop().split(".")[0];
+    return symbol === targetFruit;
+  }
+
+  function resetGame() {
+    basketCounter = 0;
+    document.getElementById("basket").textContent = "Basket: 0";
+
+    shuffleSymbols();
+
+    maxTargetCount = Math.floor(Math.random() * 14) + 2;
+    document.getElementById("maxTarget").textContent = "Max Target: " + maxTargetCount;
+
+    var targetCount1 = Math.floor(Math.random() * (maxTargetCount - 1)) + 1;
+    var targetCount2 = maxTargetCount - targetCount1;
+
+    var box3 = document.getElementById("box3");
+    box3.innerHTML = "";
+    var box2 = document.getElementById("box2");
+    box2.innerHTML = "";
+    var box1 = document.getElementById("box1");
+    box1.innerHTML = "";
+
+    createSymbol(targetFruit, targetCount1, document.getElementById("box1"), false);
+    createSymbol(targetFruit, targetCount2, document.getElementById("box2"), false);
+    createSymbol(targetFruit, 15, document.getElementById("box3"), true);
+
+    if (currentSymbolIndex === symbols.length) {
+      currentSymbolIndex = 0;
+    }
+  }
+
+  function submitAnswer() {
+    if (basketCounter === maxTargetCount) {
+      currentSymbolIndex = (currentSymbolIndex + 1) % 5;
+      // document.getElementById("symbol").textContent = "index: " + currentSymbolIndex;
+
+      switch (currentSymbolIndex) {
+        case 0:
+          targetFruit = "apple";
+          break;
+        case 1:
+          targetFruit = "banana";
+          break;
+        case 2:
+          targetFruit = "grape";
+          break;
+        case 3:
+          targetFruit = "orange";
+          break;
+        case 4:
+          targetFruit = "pencil";
+          break;
+        default:
+          targetFruit = "apple";
+      }
+
+      // document.getElementById("fruit").textContent = "fruit: " + targetFruit;
+
+      score = score + 10;
+      if (score >= highScore) {
+        highScore = score;
+        document.getElementById("highScore").textContent = "highScore: " + highScore;
+      }
+      document.getElementById("score").textContent = "Score: " + score;
+      displayTemporaryMessage("Correct Answer");
+      resetGame();
+    } else {
+      currentSymbolIndex = 0;
+      targetFruit = "apple";
+      // document.getElementById("symbol").textContent = "index: " + currentSymbolIndex;
+      score = 0;
+      document.getElementById("score").textContent = "Score: 0";
+      alert("Basket count is incorrect. Try again!");
+      resetGame();
+    }
+  }
 
   useEffect(() => {
-    updateBoxes();
-  }, [currentIndex]);
+    shuffleSymbols();
+    resetGame();
 
-  const handleNextImage = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % 1); // Only one type of image (apple)
-    updateBoxes();
-  };
+    var basketContainer = document.createElement("div");
+    basketContainer.id = "basket-container";
+    document.body.appendChild(basketContainer);
 
-  const handleDragStart = (e, id) => {
-    e.dataTransfer.setData('text/plain', id);
-  };
+    document.body.addEventListener("dragover", function (event) {
+      event.preventDefault();
+    });
 
-  const handleDrop = (e) => {
-    e.preventDefault();
-    const id = e.dataTransfer.getData('text/plain');
-    const draggedImage = document.getElementById(`draggable-apple-${id}`);
+    document.body.addEventListener("drop", drop);
+    document.body.addEventListener("drag", drag);
 
-    if (draggedImage) {
-      const rect = e.target.getBoundingClientRect();
+    return () => {
+      // Cleanup event listeners or other resources on unmount if necessary
+      document.body.removeEventListener("dragover", function (event) {
+        event.preventDefault();
+      });
 
-      // Calculate the position relative to the target element
-      const offsetX = e.clientX - rect.left;
-      const offsetY = e.clientY - rect.top;
-
-      // Set the new position of the dragged image
-      draggedImage.style.position = 'fixed'; // Use fixed position for absolute positioning on the entire page
-      draggedImage.style.left = offsetX + 'px';
-      draggedImage.style.top = offsetY + 'px';
-
-      // Append the dragged image to the document body
-      document.body.appendChild(draggedImage);
-    }
-  };
-
-  const handleDragOver = (e) => {
-    e.preventDefault();
-  };
-
-  const handleBasketDrop = (e) => {
-    e.preventDefault();
-    const id = e.dataTransfer.getData('text/plain');
-    const draggedImage = document.getElementById(`draggable-apple-${id}`);
-
-    if (draggedImage) {
-      // Remove the dragged image from the document body
-      draggedImage.parentNode.removeChild(draggedImage);
-
-      // Update the basketImages state
-      setBasketImages((prevBasketImages) => [...prevBasketImages, id]);
-    }
-  };
+      document.body.removeEventListener("drop", drop);
+      document.body.removeEventListener("drag", drag);
+    };
+  }, []);
 
   return (
     <div>
-      <h1>DragDropAdd Page</h1>
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        {/* Left Box */}
-        <div style={boxStyle}>
-          <h2>Left Box</h2>
-          {leftBoxImages.map(({ id, src }, index) => (
-            <img
-              key={id}
-              id={`draggable-apple-${id}`}
-              src={src}
-              alt={`Object ${index}`}
-              style={{ ...imageStyle, cursor: 'grab' }}
-              draggable
-              onDragStart={(e) => handleDragStart(e, id)}
-            />
-          ))}
-        </div>
-
-        {/* Plus Sign */}
-        <div style={{ width: '5%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <img src="https://cdn.onlinewebfonts.com/svg/img_25007.png" alt="Plus Sign" style={{ width: '100%', height: 'auto' }} />
-        </div>
-
-        {/* Right Box */}
-        <div style={boxStyle}>
-          <h2>Right Box</h2>
-          {rightBoxImages.map(({ id, src }, index) => (
-            <img
-              key={id}
-              id={`draggable-apple-${id}`}
-              src={src}
-              alt={`Object ${index}`}
-              style={{ ...imageStyle, cursor: 'grab' }}
-              draggable
-              onDragStart={(e) => handleDragStart(e, id)}
-            />
-          ))}
-        </div>
-
-        {/* Draggable Apples Box */}
-        <div
-          style={boxStyle}
-          onDrop={handleDrop}
-          onDragOver={handleDragOver}
-        >
-          <h2>Draggable Apples</h2>
-          {Array(15).fill(null).map((_, index) => (
-            <img
-              key={index}
-              id={`draggable-apple-${index}`}
-              src={appleImageUrl}
-              alt={`Apple ${index}`}
-              style={{ ...imageStyle, cursor: 'grab' }}
-              draggable
-              onDragStart={(e) => handleDragStart(e, index)}
-            />
-          ))}
-        </div>
+      <div className="container" id="basket">
+      
+        Basket: 0
       </div>
+      <img src="/images/basket.png" alt="Basket" className="basket-image" />
 
-      {/* Submit Button */}
-      <button onClick={handleNextImage}>Submit</button>
-
-      {/* Basket */}
-      <div
-        style={{ border: '1px solid black', padding: '10px', marginTop: '20px', minHeight: '100px' }}
-        onDrop={handleBasketDrop}
-        onDragOver={handleDragOver}
-      >
-        <h2>Basket</h2>
-        {basketImages.map((id, index) => (
-          <img
-            key={index}
-            id={`draggable-apple-${id}`}
-            src={appleImageUrl}
-            alt={`Basket Apple ${index}`}
-            style={{ ...imageStyle, cursor: 'grab' }}
-            draggable
-            onDragStart={(e) => handleDragStart(e, id)}
-          />
-        ))}
+      <div className="container" id="maxTarget">
+        Max Target: 0
       </div>
+      <div className="container" id="score">
+        Score: 0
+      </div>
+      <div className="container" id="highScore">
+        High Score: 0
+      </div>
+      <div className="container" id="box1"></div>
+      <div className="container" id="plus">
+        +
+      </div>
+      <div className="container" id="box2"></div>
+      <div className="container" id="box3"></div>
+      <div className="container" id="symbol">
+        {/* index: 0 */}
+      </div>
+      <div className="container" id="fruit">
+        {/* fruit: apple */}
+      </div>
+      <button onClick={submitAnswer}>Submit</button>
     </div>
   );
 };
 
 export default DragDropAdd;
-
-const boxStyle = {
-  border: '1px solid black',
-  padding: '10px',
-  width: '340px', // Adjust the width of the boxes as needed
-  height: '340px', // Adjust the height of the boxes as needed
-};
-
-const imageStyle = {
-  width: '40px', // Fixed size for the apple images
-  height: '40px', // Fixed size for the apple images
-  objectFit: 'contain', // Ensure the entire image fits within the specified dimensions
-};
