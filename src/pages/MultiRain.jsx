d8754f2715072e2b38dd2b98ca3a5c933870d152
Aryan <aryan.12a17@gmail.com>
2023-12-22 18:16:17 +0530

    commit2
    aqd

--- src/pages/MultiRain.jsx
+++ src/pages/MultiRain.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import './styles/MultiRain.css';
+import './styles/MultiRain.css'; // Adjust the file name as needed
 
 function MultiRain() {
   const [score, setScore] = useState(0);
   const [equations, setEquations] = useState([]);
+  const [userAnswer, setUserAnswer] = useState('');
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      const newEquation = generateEquation();
-      setEquations((prevEquations) => [...prevEquations, newEquation]);
-    }, 2000);
+    generateNewEquation();
+  }, [score]); // Re-generate equation when the score changes
 
-    return () => clearInterval(intervalId);
-  }, []);
+  const generateNewEquation = () => {
+    const newEquation = generateEquation();
+    setEquations([newEquation]);
+  };
 
   const generateEquation = () => {
     const num1 = Math.floor(Math.random() * 10) + 1;
@@ -22,39 +23,57 @@ function MultiRain() {
     return {
       equation: `${num1} X ${num2}`,
       answer,
+      column: Math.floor(Math.random() * 10),
+      row: Math.floor(Math.random() * 10),
     };
   };
 
-  const handleAnswer = (userAnswer, index) => {
-    const currentEquation = equations[index];
+  const handleAnswer = () => {
+    const currentEquation = equations[0];
 
-    if (userAnswer === currentEquation.answer) {
+    if (parseInt(userAnswer, 10) === currentEquation.answer) {
       setScore(score + 1);
+     
     } else {
       setScore(Math.max(score - 1, 0));
     }
 
-    const newEquations = [...equations];
-    newEquations.splice(index, 1, generateEquation());
-    setEquations(newEquations);
+    setUserAnswer(''); // Clear the answer after handling it
   };
 
   return (
     <div className="MultiRain">
       <h1>MultiRain</h1>
       <div className="game-container">
-        {equations.map((equation, index) => (
-          <div key={index} className="equation" style={{ animationDelay: `${index * 2}s` }}>
-            {equation.equation}
-            <input
-              type="number"
-              placeholder="Your Answer"
-              onChange={(e) => handleAnswer(parseInt(e.target.value, 10), index)}
-            />
-          </div>
+        {[...Array(10)].map((_, rowIndex) => (
+          <React.Fragment key={rowIndex}>
+            {[...Array(10)].map((_, colIndex) => (
+              <div
+                key={`${rowIndex}-${colIndex}`}
+                className={`cell ${colIndex === 9 ? 'last-column' : ''}`}
+              >
+                {equations
+                  .filter((equation) => equation.row === rowIndex && equation.column === colIndex)
+                  .map((equation, index) => (
+                    <div key={index} className="equation" style={{ animationDelay: `${index * 2}s` }}>
+                      {equation.equation}
+                    </div>
+                  ))}
+              </div>
+            ))}
+            {/* Display row index at the rightmost column */}
+            <div className="row-index">{10-rowIndex}</div>
+          </React.Fragment>
         ))}
       </div>
       <div className="score">Score: {score}</div>
+      <input
+        type="number"
+        placeholder="Your Answer"
+        value={userAnswer}
+        onChange={(e) => setUserAnswer(e.target.value)}
+        onBlur={handleAnswer}
+      />
     </div>
   );
 }
